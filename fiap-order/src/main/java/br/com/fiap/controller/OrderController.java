4d8d41fd1bf8a16e4f9c497b022bf801477c6409
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2023-10-18 01:42:44 -0300

    create fiap-order service and Dockerfile

---
+++ fiap-order/src/main/java/br/com/fiap/controller/OrderController.java
@@ -0,0 +1,16 @@
+package br.com.fiap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    OrderController(){}
+@GetMapping
+    public String hello(){
+        return "Order test return";
+    }
+
+}
