c764dd5101a524dc85b7b6ba9b9fdc3ff9cfd844
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2023-10-19 23:08:47 -0300

    Implements InsertOrderAdapter

--- fiap-order/src/main/java/br/com/fiap/adapters/out/InsertOrderAdapter.java
+++ fiap-order/src/main/java/br/com/fiap/adapters/out/InsertOrderAdapter.java
@@ -1,6 +1,7 @@
 package br.com.fiap.adapters.out;
 
 import br.com.fiap.adapters.out.repository.IOrderRepository;
+import br.com.fiap.adapters.out.repository.mapper.IOrderEntityMapper;
 import br.com.fiap.application.core.domain.Order;
 import br.com.fiap.application.ports.out.IInsertOrderOutputPort;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,16 @@ import org.springframework.stereotype.Component;
 public class InsertOrderAdapter implements IInsertOrderOutputPort {
     @Autowired
     IOrderRepository iOrderRepository;
+    @Autowired
+    IOrderEntityMapper iOrderEntityMapper;
+
     @Override
     public void insertOrder(Order order) {
-    //iOrderRepository.save();
+        /*
+        Creating a mapper to transform the customer
+        into a customerEntity to be able to persist in the base
+         */
+        var orderEntity = iOrderEntityMapper.toOrderEntity(order);
+        iOrderRepository.save(orderEntity);
     }
 }
