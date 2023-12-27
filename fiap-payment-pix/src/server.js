83de3e69faaf3c818b662697b74c4046ca3e6691
mateusValentim <mateus.valentim19@hotmail.com>
2023-12-26 23:40:16 -0300

    Criaçao do projeto fiap-payment-pix em nodeJs

---
+++ fiap-payment-pix/src/server.js
@@ -0,0 +1,6 @@
+console.log(process.log)
+if(process.env.NODE_ENV !== 'production'){
+require('dotenv').config
+}
+
+console.log(process.env)
--- fiap-payment/src/main/java/br/com/fiap/adapters/out/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.adapters.out.repository;
-
-import br.com.fiap.application.core.domain.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
