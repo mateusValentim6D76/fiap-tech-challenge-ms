dafaea492df4d80aeb9541d55784baf1a79b4434
mateusValentim <mateus.valentim19@hotmail.com>
2023-12-27 21:56:42 -0300

    Adicionando o token do tipo base

--- fiap-payment-pix/src/server.js
+++ fiap-payment-pix/src/server.js
@@ -8,12 +8,15 @@ const path = require('path');
 const https = require('https')
 
 const cert = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
-)
+    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`))
 const agent = new https.Agent({
     pfx: cert,
     passphrase: ""
 })
+
+const credentials = Buffer.from(`${process.env.GN_CLIENT_ID}: ${process.env.GN_CLIENT_SECRET}`)
+.toString('base64');
+
 
 
 
