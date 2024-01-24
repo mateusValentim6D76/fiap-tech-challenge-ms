d9a5486b00c3693991a5e7fd0bec0fc8e92cd3f6
mateusValentim <mateus.valentim19@hotmail.com>
2024-01-23 22:58:15 -0300

    first commit

---
+++ fiap-notification/src/main/java/br/com/fiap/notification/application/domain/model/Notification.java
@@ -0,0 +1,10 @@
+package br.com.fiap.notification.application.domain.model;
+
+public class Notification {
+
+    private static final String RECEIVED = "Your order has been received";
+    private static final String IN_PREPARATION = "Your order is being prepared";
+    private static final String READY = "Your order is ready";
+    private static final String FINISHED = "Your order is finalized";
+
+}
